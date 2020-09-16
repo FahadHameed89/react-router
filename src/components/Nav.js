0096d243ebca29848750dcba539dd720e4fc273e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-16 11:31:22 -0600

    Setting up nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Nav ()
+{
+    return (
+        <nav>
+            <h2>To-Do App Navigation</h2>
+            <ul>
+                <li>
+                    
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
+
