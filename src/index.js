e9b1f843612880c021cba1045312640c26abc191
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-16 11:27:19 -0600

    Instealled react-router-dom and added a Router and Routes to the index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { addNewToDo } from './actions/todos';
 import { Provider } from 'react-redux';
 import ToDos from './components/ToDos';
 import ToDoList from './components/ToDoList';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 /**
  * Redux Store
@@ -44,10 +44,9 @@ ReactDOM.render(
   <Provider store={store}>
     <h1>React-Redux To-Do List (TECHCareers)</h1>
     <Router>
-
+      <Route path="/" component={ToDoList} />
+      <Route path="/form" component={ToDos} />
     </Router>
-    <ToDos />
-    <ToDoList />
   </Provider>,
   document.getElementById('root')
 );
